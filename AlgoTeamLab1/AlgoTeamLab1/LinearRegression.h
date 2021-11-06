d2e382896aa43f74514785c3849905c23d001f7f
Maksym Havtylyuk <12u18m29mm@gmail.com>
2021-11-06 22:58:27 +0200

    Додав опис алгоритму

--- AlgoTeamLab1/AlgoTeamLab1/LinearRegression.h
+++ AlgoTeamLab1/AlgoTeamLab1/LinearRegression.h
@@ -1,2 +1,24 @@
 #pragma once
+//Done by Maksym Havrilyuk
+#include "Matrix.h"
+#include "RationalNum.h"
+#include "InverseLUDecomposition.h"
+#include <vector>
+
+/*
+Лінійна регресія використовується для відновлення лінійного
+співвідношення між скалярною залежною величиною та вектором незалежних змінних
+В матричному вигляді Y = XB + C,
+де Y - вектор значень залежних змінних
+   Х - матриця значень незалежних змінних
+   B - вектор коефіцієнтів
+   С - вектор довільних похибок
+Мета - відновлення вектора В за заданими У та Х
+Використовується метод найменших квадратів:
+В = ((X'X)^-1)X'Y
+*/
+vector<RationalNum> LinearRegression(vector<RationalNum> Y, Matrix<RationalNum> X)
+{
+
+}
 
